3eb074d05550710e999311d8f50d561865a45740
lelido <17890168+lelido@users.noreply.github.com>
2025-03-25 18:01:57 +0900

    Add logic to get the computer choice

---
+++ script.js
@@ -0,0 +1,14 @@
+function getComputerChoice() {
+    const randomChoice = Math.floor(Math.random() * 3);
+
+    switch (randomChoice) {
+        case 0:
+            return "rock";
+        case 1:
+            return "paper";
+        case 2:
+            return "scissors";
+        default:
+            return "Unknown choice";
+    }
+}
